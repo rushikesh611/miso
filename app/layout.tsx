375c5691c5e6d63c12a26572277a57d42b0492d2
Hrushikesh Jadhav <rushi611@gmail.com>
2023-04-20 12:21:33 +0530

    init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+export const metadata = {
+  title: "Miso",
+  description: "An airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="{{font.className}}">{children}</body>
+    </html>
+  );
+}
